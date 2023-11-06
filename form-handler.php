e77ac755a239ff0e7e98acb315aa8e3f69cb31bc
Blessing Awak <melissasignature1@gmail.com>
2023-11-06 19:06:31 +0100

    commit

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'uniport.edu.ng';
+$email_from = 'cookingwithmelissa@gmail.com';
 
 $email_subject = 'New Form Submission';
 
